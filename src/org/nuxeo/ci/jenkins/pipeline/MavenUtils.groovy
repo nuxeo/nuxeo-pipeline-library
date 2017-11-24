2628b0fde7ea407387bb3cb96de4248b7c00039c
Pierre-Gildas MILLON <pgmillon@nuxeo.com>
2017-11-24 18:46:56 +0100

    @NonCPS

--- src/org/nuxeo/ci/jenkins/pipeline/MavenUtils.groovy
+++ src/org/nuxeo/ci/jenkins/pipeline/MavenUtils.groovy
@@ -2,6 +2,7 @@ package org.nuxeo.ci.jenkins.pipeline
 
 class MavenUtils {
 
+    @NonCPS
     static def getMavenVersion(String pomString) {
         return new XmlSlurper().parse(new StringReader(pomString)).version
     }
--- src/org/nuxeo/ci/jenkins/pipeline/ReleaseUtils.groovy
+++ src/org/nuxeo/ci/jenkins/pipeline/ReleaseUtils.groovy
@@ -2,6 +2,7 @@ package org.nuxeo.ci.jenkins.pipeline
 
 class ReleaseUtils {
 
+    @NonCPS
     static def readLog(String log) {
         return new ConfigSlurper().parse(log).flatten()
     }
