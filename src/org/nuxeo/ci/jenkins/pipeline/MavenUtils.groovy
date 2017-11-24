24ae2fd4d4b1653c7f6dc03c9af93ee2f746bfd3
Pierre-Gildas MILLON <pgmillon@nuxeo.com>
2017-11-24 19:26:46 +0100

    cast return value as String

--- src/org/nuxeo/ci/jenkins/pipeline/MavenUtils.groovy
+++ src/org/nuxeo/ci/jenkins/pipeline/MavenUtils.groovy
@@ -3,7 +3,7 @@ package org.nuxeo.ci.jenkins.pipeline
 class MavenUtils implements Serializable {
 
     static def getMavenVersion(String pomString) {
-        return new XmlSlurper().parse(new StringReader(pomString)).version
+        return (String) new XmlSlurper().parse(new StringReader(pomString)).version
     }
 
 }
