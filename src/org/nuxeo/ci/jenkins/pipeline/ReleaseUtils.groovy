c944460ef6f8c0dd573df9d6ecee848b08ab6788
Pierre-Gildas MILLON <pgmillon@nuxeo.com>
2017-11-25 00:07:51 +0100

    Revert "Set return type instead of cast"
    This reverts commit 36944d6968ddf64beb2696e3fb37d7a113491fcc.

--- src/org/nuxeo/ci/jenkins/pipeline/ReleaseUtils.groovy
+++ src/org/nuxeo/ci/jenkins/pipeline/ReleaseUtils.groovy
@@ -2,7 +2,7 @@ package org.nuxeo.ci.jenkins.pipeline
 
 class ReleaseUtils implements Serializable {
 
-    static String readLog(String log) {
+    static def readLog(String log) {
         return new ConfigSlurper().parse(log).flatten()
     }
 }
