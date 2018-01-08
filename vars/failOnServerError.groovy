bafca5a29d76ae26b77309353bb6c2a2de271542
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2018-01-08 15:20:26 +0100

    NXBT-2129 refined comments and published setBuildStatus as a step

--- vars/failOnServerError.groovy
+++ vars/failOnServerError.groovy
@@ -1,8 +1,13 @@
 /**
  * grep for errors in server logs throwing an abort exception
+ * as an alternative to https://wiki.jenkins.io/display/JENKINS/Text-finder+Plugin which
+ * does not provided a dedicated step for yet.
+ *
+ * filepath -> path to the file being searched
+ * pattern -> regexp pattern which defaults to log4j error pattern
  */
-def call(String path, String pattern = '^[0-9]{4}-[0-9]{2}-[0-9]{2}.*ERROR.*') {
+def call(String filepath, String pattern = '^[0-9]{4}-[0-9]{2}-[0-9]{2}.*ERROR.*') {
   sh """#!/bin/bash -ex
-        ! grep -E '$pattern' $path
+        ! grep -E '$pattern' $filepath
   """
 }
--- vars/withBuildStatus.groovy
+++ vars/withBuildStatus.groovy
@@ -1,30 +1,17 @@
+#!groovy
+
 /**
  * body's execution control that propagates build statuses to github according to the result
  */
-def call(String context, String repourl, String sha, Closure body) {
+def call(String context, String repourl, String sha, String backref, Closure body) {
   currentBuild.result = 'SUCCESS'
-  setBuildStatus("", "PENDING", context, repourl, sha)
+  setBuildStatus("", "PENDING", context, repourl, sha, backref)
   try {
     body.call()
-    setBuildStatus("", "SUCCESS", context, repourl, sha)
+    setBuildStatus("", "SUCCESS", context, repourl, sha, backref)
   } catch (Throwable cause) {
-    setBuildStatus(cause.toString().take(140), "FAILURE", context, repourl, sha)
+    setBuildStatus(cause.toString().take(140), "FAILURE", context, repourl, sha, backref)
     throw cause
   }
-}
-
-/**
- * https://wiki.jenkins.io/display/JENKINS/GitHub+Plugin
- */
-def setBuildStatus(String message, String state, String context, String repourl, String sha) {
-  step([
-      $class             : "GitHubCommitStatusSetter",
-      reposSource        : [$class: "ManuallyEnteredRepositorySource", url: repourl],
-      contextSource      : [$class: "ManuallyEnteredCommitContextSource", context: context],
-      errorHandlers      : [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
-      commitShaSource    : [$class: "ManuallyEnteredShaSource", sha: sha],
-      statusBackrefSource: [$class: "ManuallyEnteredBackrefSource", backref: "${BUILD_URL}"],
-      statusResultSource : [$class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]]]
-  ]);
 }
 
