919ba210a1dbc5991045c93668dd281388a44a81
alexistimic <atimic@nuxeo.com>
2018-05-07 17:19:07 +0200

    wip

--- vars/withDiscardProperties.groovy
+++ vars/withDiscardProperties.groovy
@@ -3,6 +3,6 @@
   * To be placed before the "stage phases" of the job
 **/
 
-def withDiscardProperties (
+def withDiscardProperties() {
 	properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '1', daysToKeepStr: '60', numToKeepStr: '60']]])
-)
+}
