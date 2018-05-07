b5b7fdc7e2b4e95abe74a17ae142414555505473
alexistimic <atimic@nuxeo.com>
2018-05-07 18:20:56 +0200

    wip

--- vars/withDiscardProperties.groovy
+++
@@ -1,8 +0,0 @@
-/**
-  * Set Discard old builds properties
-  * To be placed before the "stage phases" of the job
-**/
-
-def call() {
-	properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '1', daysToKeepStr: '60', numToKeepStr: '60']]])
-}
