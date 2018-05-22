fb409675a76faba3205ca960bec2a5d5cf4a66f7
alexistimic <atimic@nuxeo.com>
2018-05-22 18:16:30 +0200

    wip

--- vars/setDiscardProperties.groovy
+++ vars/setDiscardProperties.groovy
@@ -1,22 +1,12 @@
+#!groovy
 /**
   * Set Discard old builds properties
   * To be placed before the "stage phases" of the job
 **/
 
-import jenkins.model.*;
-import hudson.model.*;
-import hudson.maven.*;
-import hudson.tasks.*;
-
-def build = Thread.currentThread().toString()
-def regexp= ".+?/job/([^/]+)/.*"
-def match = build  =~ regexp
-def jobName = match[0][1]
-println(jobName)
-
 def call(String artiDaysToKeep, String artiNumToKeep, String daysToKeep, String numToKeep ) {
 
-	String jobName = System.getenv('JOB_NAME')
-	println(jobName + "\n\n\n\n")
-	properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: artiDaysToKeep, artifactNumToKeepStr: artiNumToKeep, daysToKeepStr: daysToKeep, numToKeepStr: numToKeep]]])
+	properties([
+	[$class: 	'BuildDiscarderProperty', 
+	strategy: 	[$class: 'LogRotator', artifactDaysToKeepStr: artiDaysToKeep, artifactNumToKeepStr: artiNumToKeep, daysToKeepStr: daysToKeep, numToKeepStr: numToKeep]]])
 }
