d207900eac9f004f47b719009dfcfb0c3ab6d28d
alexistimic <atimic@nuxeo.com>
2018-05-22 18:01:37 +0200

    wip

--- vars/setDiscardProperties.groovy
+++ vars/setDiscardProperties.groovy
@@ -15,5 +15,12 @@ def jobName = match[0][1]
 println(jobName)
 
 def call(String artiDaysToKeep, String artiNumToKeep, String daysToKeep, String numToKeep ) {
+
+	def build = Thread.currentThread().toString()
+	def regexp= ".+?/job/([^/]+)/.*"
+	def match = build  =~ regexp
+	def jobName = match[0][1]
+	println(jobName)
+
 	properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: artiDaysToKeep, artifactNumToKeepStr: artiNumToKeep, daysToKeepStr: daysToKeep, numToKeepStr: numToKeep]]])
 }
