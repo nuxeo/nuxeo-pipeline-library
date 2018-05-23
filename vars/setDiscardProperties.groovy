52e91055ee976f6bef90541627723ada71b9411f
alexistimic <atimic@nuxeo.com>
2018-05-23 17:04:00 +0200

    wip

--- vars/setDiscardProperties.groovy
+++ vars/setDiscardProperties.groovy
@@ -2,11 +2,16 @@
 /**
   * Set Discard old builds properties
   * To be placed before the "stage phases" of the job
+  * Overrides all set current job properties 
 **/
 
 def call(String artiDaysToKeep, String artiNumToKeep, String daysToKeep, String numToKeep ) {
-	
-	properties([
-	[$class: 	'BuildDiscarderProperty', 
-	strategy: 	[$class: 'LogRotator', artifactDaysToKeepStr: artiDaysToKeep, artifactNumToKeepStr: artiNumToKeep, daysToKeepStr: daysToKeep, numToKeepStr: numToKeep]]])
+
+	if (properties([
+        [$class:        'BuildDiscarderProperty',
+        strategy:       [$class: 'LogRotator', artifactDaysToKeepStr: NULL, artifactNumToKeepStr: NULL, daysToKeepStr: NULL, numToKeepStr: NULL]]]))	{	 
+		properties([
+		[$class: 	'BuildDiscarderProperty', 
+		strategy: 	[$class: 'LogRotator', artifactDaysToKeepStr: artiDaysToKeep, artifactNumToKeepStr: artiNumToKeep, daysToKeepStr: daysToKeep, numToKeepStr: numToKeep]]])
+	}
 }
