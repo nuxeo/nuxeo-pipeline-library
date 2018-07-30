17c06b83a07e11b6905131536a5392ecc6ef3b5f
alexistimic <atimic@nuxeo.com>
2018-07-30 16:25:06 +0200

    wip

--- vars/setDiscardProperties.groovy
+++ vars/setDiscardProperties.groovy
@@ -5,6 +5,28 @@
   * Overrides all set current job properties 
 **/
 
+import jenkins.model.*;
+import hudson.model.*;
+import hudson.maven.*;
+import hudson.tasks.*;
+import com.cloudbees.hudson.plugins.folder.*;
+import org.jenkinsci.plugins.workflow.multibranch.*;
+import com.cloudbees.hudson.plugins.modeling.impl.auxiliary.*;
+import com.cloudbees.hudson.plugins.modeling.impl.jobTemplate.*;
+import jenkins.branch.*;
+import org.jenkinsci.plugins.workflow.job.*;
+import javax.mail.internet.*;
+import javax.mail.*;
+import javax.activation.*;
+import hudson.tasks.MailSender;
+import jenkins.plugins.mailer.tasks.MimeMessageBuilder;
+import java.util.*;
+import hudson.tools.*;
+import com.github.mjdetullio.jenkins.plugins.multibranch.FreeStyleMultiBranchProject;
+import com.github.mjdetullio.jenkins.plugins.multibranch.MavenMultiBranchProject;
+import hudson.matrix.*;
+
+
 def call(String artiDaysToKeep, String artiNumToKeep, String daysToKeep, String numToKeep ) {
 
     test = currentJob.getName();
