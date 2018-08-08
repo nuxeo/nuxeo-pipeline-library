95e6d9bb63c0f47417271fdaed5a183672463638
alexistimic <atimic@nuxeo.com>
2018-08-08 10:14:31 +0200

    wip

--- vars/setDiscardProperties.groovy
+++ vars/setDiscardProperties.groovy
@@ -9,7 +9,7 @@
 def call(String artiDaysToKeep, String artiNumToKeep, String daysToKeep, String numToKeep ) {
 
     discardList = []
-    discardList.push(BuildDiscarderProperty(strategy(setDiscardProperties(
+    discardList.push(BuildDiscarderProperty(strategy(setDiscardProperties.call(
             artifactDaysToKeepStr: '',
             artifactNumToKeepStr: '1',
             daysToKeepStr: '60',
